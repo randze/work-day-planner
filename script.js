5c7c399f08e95a6a5a4c04571274ac7f4c27b6ab
randze <65006180+randze@users.noreply.github.com>
2020-06-11 19:34:26 -0400

    Finished saving function

--- script.js
+++ script.js
@@ -1,136 +1,62 @@
-// Array to generate BlankPage
-const genTT = [
-{   time: '9am',
-    task: ''},
-{   time: '10am', 
-    task: ''},
-{   time: '11am', 
-    task: ''},
-{   time: '12pm', 
-    task: ''},
-{   time: '1pm', 
-    task: ''},
-{   time: '2pm', 
-    task: ''},
-{   time: '3pm', 
-    task: ''},
-{   time: '4pm', 
-    task: ''},
-{   time: '5pm', 
-    task: ''}
-]
+// Time for Table
+const scheduleTime = ["12am","1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm"]
+
+// Check if there's saved storage
+let savedSchedule = localStorage.savedSchedule ? JSON.parse(localStorage.savedSchedule) : [];
+let checkSave = localStorage.savedSchedule.length > 0 ? true : false
 
 // Checks and Displays Date
 const currentDate = moment().format('dddd MMM Do'); 
     document.querySelector('#currentDay').textContent = `${currentDate}`
 
+// Get current past hours to check against time
 const checkTime = new Date().getHours();
 
-function genBlank() {
-    for (var i = 0 ; i < genTT.length ; i++ ) {
+// Generate Schedule
+function genPage() {
+    // Clear Page
+    document.querySelector('#myTimeTable').innerHTML = `` 
+
+    for (var idx = 9 ; idx <18 ; idx++ ) {
         document.querySelector('#myTimeTable').innerHTML += 
         `
         <tr>
-            <th class="text-right" scope="row">${genTT[i].time}</th>
-            <td id="_${genTT[i].time}" class="align-middle">${genTT[i].task}</td>
+            <th class="text-right" scope="row">${scheduleTime[idx]}</th>
+            <td class="align-middle"><form>
+                <div class="form-group">
+                    <input type="text" class="form-control task${idx}" id="" placeholder="" value="${savedSchedule[idx] ? savedSchedule[idx] : ``}">
+                </div>
+            </td>
             <td class="align-middle text-right">
               <div class="btn-group" style="">
-                <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editPopup">Add/Edit</button>
-                <button type="button" class="btn btn-primary btn-danger btn-sm">X</button>
+                <button onclick="saveItem(event)" type="button" class="btn btn-primary btn-sm">Save</button>
               </div>
             </td>
         </tr>
         `
-    }
-}
-
-function timeCheck() {
-    for (var i = 0 ; i < genTT.length ; i++ ) {
-        switch(genTT[i].time) {
-            case '9am':
-                if (checkTime == 9) {
-                    document.querySelector("#_9am").classList.add("present");
-                } else if (checkTime > 9) {
-                    document.querySelector("#_9am").classList.add("past");
-                } else if (checkTime < 9) {
-                    document.querySelector("#_9am").classList.add("future");
-                }
-                break;
-            case '10am':
-                if (checkTime == 10) {
-                    document.querySelector("#_10am").classList.add("present");
-                } else if (checkTime > 10) {
-                    document.querySelector("#_10am").classList.add("past");
-                } else if (checkTime < 10) {
-                    document.querySelector("#_10am").classList.add("future");
-                }
-                break;
-            case '11am':
-                if (checkTime == 11) {
-                    document.querySelector("#_11am").classList.add("present");
-                } else if (checkTime > 11) {
-                    document.querySelector("#_11am").classList.add("past");
-                } else if (checkTime < 11) {
-                    document.querySelector("#_11am").classList.add("future");
-                }
-                break;
-            case '12pm':
-                if (checkTime == 12) {
-                    document.querySelector("#_12pm").classList.add("present");
-                } else if (checkTime > 12) {
-                    document.querySelector("#_12pm").classList.add("past");
-                } else if (checkTime < 12) {
-                    document.querySelector("#_12pm").classList.add("future");
-                }
-                break;
-            case '1pm':
-                if (checkTime == 13) {
-                    document.querySelector("#_1pm").classList.add("present");
-                } else if (checkTime > 13) {
-                    document.querySelector("#_1pm").classList.add("past");
-                } else if (checkTime < 13) {
-                    document.querySelector("#_1pm").classList.add("future");
-                }
-                break;
-            case '2pm':
-                if (checkTime == 14) {
-                    document.querySelector("#_2pm").classList.add("present");
-                } else if (checkTime > 14) {
-                    document.querySelector("#_2pm").classList.add("past");
-                } else if (checkTime < 14) {
-                    document.querySelector("#_2pm").classList.add("future");
-                }
-                break;
-            case '3pm':
-                if (checkTime == 15) {
-                    document.querySelector("#_3pm").classList.add("present");
-                } else if (checkTime > 15) {
-                    document.querySelector("#_3pm").classList.add("past");
-                } else if (checkTime < 15) {
-                    document.querySelector("#_3pm").classList.add("future");
-                }
-                break;
-            case '4pm':
-                if (checkTime == 16) {
-                    document.querySelector("#_4pm").classList.add("present");
-                } else if (checkTime > 16) {
-                    document.querySelector("#_4pm").classList.add("past");
-                } else if (checkTime < 16) {
-                    document.querySelector("#_4pm").classList.add("future");
-                }
-                break;
-            case '5pm':
-                if (checkTime == 17) {
-                    document.querySelector("#_5pm").classList.add("present");
-                } else if (checkTime > 17) {
-                    document.querySelector("#_5pm").classList.add("past");
-                } else if (checkTime < 17) {
-                    document.querySelector("#_5pm").classList.add("future");
-                }
-                break;
+        // check time color
+        if (checkTime == idx) {
+            document.querySelector(`.task${idx}`).classList.add("present");
+        } else if (checkTime > idx) {
+            document.querySelector(`.task${idx}`).classList.add("past");
+        } else if (checkTime < idx) {
+            document.querySelector(`.task${idx}`).classList.add("future");
         }
     }
 }
 
-genBlank()
-timeCheck()
+
+
+// function to save data to localstorage
+function saveItem(event) {
+    event.preventDefault();
+
+    for (var idx = 9 ; idx < 18 ; idx++) {
+    savedSchedule[idx] = document.querySelector(`.task${idx}`).value
+    }
+
+    localStorage.savedSchedule = JSON.stringify(savedSchedule)
+    console.log('Saved schedule to localstorage !')
+}
+
+genPage ()
